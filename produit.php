21774f0236333d9fb72c93479f6cf38010cea5a6
Jerome Byte <akotayaojerome@gmail.com>
2025-10-16 11:44:14 +0200

    correction de la taille de l'image

--- produit.php
+++ produit.php
@@ -189,8 +189,9 @@ user-select: none;
     <div class="col-md-8">
         <div class="card shadow-sm" >
             
-            <img src="<?= $produit_principal->image ?>" style="width: 100%">
-            <h3 align="center"><?= $produit_principal->nom ?></h3>
+        <img src="<?= $produit_principal->image ?>" 
+            style="width: 60%; max-height: 500px; object-fit: contain; display: block; margin: 0 auto;">           
+             <h3 align="center"><?= $produit_principal->nom ?></h3>
 
             <div class="card-body">
                 <p class="card-text"><?= $produit_principal->description ?></p>
